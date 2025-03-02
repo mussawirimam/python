a5c2628982f6306151f1b6af123763574c69ff15
mussawirimam <46902028+mussawirimam@users.noreply.github.com>
2025-03-02 16:53:49 -0500

    Create returnmultiplevaluesusingtuples.py

---
+++ returnmultiplevaluesusingtuples.py
@@ -0,0 +1,6 @@
+def min_max(numbers):
+    return min(numbers), max(numbers)
+
+minimum, maximum = min_max([1, 5, 3, 9, 2])
+print(f"Minimum: {minimum}, Maximum: {maximum}")
+#Minimum: 1, Maximum: 9
