e7f0e674b0f798be5992fb6ebf4a39c84bbb4e8d
mussawirimam <46902028+mussawirimam@users.noreply.github.com>
2025-03-02 16:28:59 -0500

    Create greetfunction.py

---
+++ greetfunction.py
@@ -0,0 +1,10 @@
+def greet(name, age):
+    name = input("enter your name: ")
+    age = input("enter your age: ")
+    print(age, name)
+#   return f"hello, your name is {name}, your age is {age}"
+    
+greet("name", "age")
+#greet("name", "age")
+#print(greet("name", "age"))
+
