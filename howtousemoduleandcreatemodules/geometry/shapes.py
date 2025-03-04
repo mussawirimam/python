2f8c4a638b313820fd18d90a58dba8e84ec88053
mussawirimam <46902028+mussawirimam@users.noreply.github.com>
2025-03-03 21:13:54 -0500

    Create shapes.py

---
+++ howtousemoduleandcreatemodules/geometry/shapes.py
@@ -0,0 +1,12 @@
+# geometry/shapes.py
+
+import math
+
+def circle_area(radius):
+    return math.pi * radius ** 2
+
+def rectangle_area(length, width): # we are setting the parameter in the function called rectangle_area
+    return length * width # this is the argument that where we can do the computation (basically used as the phantom variable when we call this function)
+
+def triangle_area(base, height):
+    return 0.5 * base * height
