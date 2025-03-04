561665583e10024ff6e87c173bb6642c86948dd5
mussawirimam <46902028+mussawirimam@users.noreply.github.com>
2025-03-03 21:06:49 -0500

    Create advanced_math.py

---
+++ howtousemoduleandcreatemodules/advanced_math.py
@@ -0,0 +1,15 @@
+# advanced_math.py
+
+import math
+
+def square_root(x):
+    return math.sqrt(x)
+
+def power(base, exponent):
+    return math.pow(base, exponent)
+
+def sin(angle):
+    return math.sin(math.radians(angle))
+
+def cos(angle):
+    return math.cos(math.radians(angle))
