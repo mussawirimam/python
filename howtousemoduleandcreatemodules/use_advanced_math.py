64b2973315608316001254d4b5e7d9db75e2c2f7
mussawirimam <46902028+mussawirimam@users.noreply.github.com>
2025-03-03 21:07:11 -0500

    Create use_advanced_math.py

---
+++ howtousemoduleandcreatemodules/use_advanced_math.py
@@ -0,0 +1,13 @@
+# use_advanced_math.py
+
+from advanced_math import square_root, power
+from advanced_math import sin as sine, cos as cosine
+
+x = 16
+y = 2
+angle = 30
+
+print(f"Square root of {x}: {square_root(x)}")
+print(f"{x} to the power of {y}: {power(x, y)}")
+print(f"Sine of {angle} degrees: {sine(angle)}")
+print(f"Cosine of {angle} degrees: {cosine(angle)}")
