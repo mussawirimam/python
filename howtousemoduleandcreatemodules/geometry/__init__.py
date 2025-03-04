68116f7dbe2e7a5e11b64eff01e75a8bc1043563
mussawirimam <46902028+mussawirimam@users.noreply.github.com>
2025-03-03 21:10:40 -0500

    Create __init__.py

---
+++ howtousemoduleandcreatemodules/geometry/__init__.py
@@ -0,0 +1,4 @@
+#touch ~/project/geometry/__init__.py
+#touch ~/project/geometry/shapes.py
+#The __init__.py file is required to make Python treat the directory as a package.
+#It can be empty or contain initialization code for the package.
